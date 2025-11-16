79413cb0e7ac6bc20568afd224be3a1662aefecb
Chandan das <chandandas@Chandans-MacBook-Air.local>
2025-11-16 14:43:51 +0530

    Prepare frontend for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',           // Change output folder from 'dist' to 'build'
+    chunkSizeWarningLimit: 1000 // Optional: avoid warnings for large JS chunks
+  }
+});
